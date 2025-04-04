d5604a948e36f447f0e83ee8770c3809a562b505
DiegoA <diegoaneli@gmail.com>
2025-04-04 16:01:27 +0200

    test

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="bg-beige text-grafite text-center py-6 text-sm mt-12 border-t">
-        © {new Date().getFullYear()} Pan di20 · Tutti i diritti riservati
+        © {new Date().getFullYear()} Pan di 20 · Tutti i diritti riservati
       </footer>
     );
   }
